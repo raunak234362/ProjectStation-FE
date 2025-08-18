bc570618fe080bb252da3138139065c433a78842
Raunak <raunakdob@gmail.com>
2025-08-18 15:01:57 +0530

    feat: Change PWA registration type to prompt and add new Sentry breadcrumb log

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
       workbox: {
         maximumFileSizeToCacheInBytes: 12 * 1024 * 1024, // 12 MB
