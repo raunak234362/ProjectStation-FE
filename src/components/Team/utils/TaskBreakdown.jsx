15315a83d4c6d3e6c4749261765a144a0068558f
Raunak <raunakdob@gmail.com>
2025-10-14 20:29:10 +0530

    Enhance TaskBreakdown component by adding a new status case for "ASSIGNED" to improve task status representation; ensure consistent styling for assigned tasks in the UI.

--- src/components/Team/utils/TaskBreakdown.jsx
+++ src/components/Team/utils/TaskBreakdown.jsx
@@ -56,6 +56,8 @@ const TasksBreakdown = ({ tasks, parseDurationToMinutes }) => {
                             ? "bg-green-100 text-green-800"
                             : task.status === "IN_PROGRESS"
                             ? "bg-blue-100 text-blue-800"
+                            : task.status === "ASSIGNED"
+                            ? "bg-blue-100 text-blue-800"
                             : task.status === "IN_REVIEW"
                             ? "bg-yellow-100 text-yellow-800"
                             : "bg-gray-100 text-gray-800"
