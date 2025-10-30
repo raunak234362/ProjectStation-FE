1a24e75aa48762bf74e945e3e0ea2a375824e8e7
Raunak <raunakdob@gmail.com>
2025-10-30 12:28:46 +0530

    Added new access role

--- src/util/getUserType.jsx
+++ src/util/getUserType.jsx
@@ -1,9 +1,9 @@
 const getUserType = (user) => {
   if (user.role === "STAFF") {
     if (user.is_est && user.is_superuser) return "estimator";
+    if (user.is_pmo) return "PMO"
     if (user.is_superuser) return "admin";
     if (user.is_supermanager) return "deputy-manager";
-    if (user.is_pmo) return "PMO"
     if (user.is_sales) return "sales";
     if (user.is_estHead) return "estimator-head";
     if (user.is_staff && user.is_manager) return "department-manager";
